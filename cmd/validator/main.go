5b111e2776a85d9d2539a9a1bbcef08f67fd9a93
Vladimir Machulin <mvv00122@outlook.com>
2026-09-07 14:08:06 +0300

    Add grpc server entry point

--- cmd/validator/main.go
+++ cmd/validator/main.go
@@ -1,1 +1,28 @@
-package validator
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/vvvvgross/email-validator/api/validator_v1"
+	"github.com/vvvvgross/email-validator/internal/validator"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to start listener: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	myServer := &validator.Server{}
+	pb.RegisterEmailValidationServer(grpcServer, myServer)
+
+	log.Println("Starting gRPC server on port :50051...")
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
